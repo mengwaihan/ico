22e2fd0f9cba27d74edd78078fc1353e16ae0424
Frank <frankz@eyebuydirect.com>
2017-09-08 17:47:07 +0800

    test

--- cron.php
+++ cron.php
@@ -4,21 +4,20 @@ require_once ('init.php');
 
 //获取当前价格A
 
-$params = array('symbol' => 'ltc_cny');
+$params = array('symbol' => 'eth_cny');
 $result = $ico -> tickerApi($params);
 print_r($result);
 
 $A = $result->ticker;
-exit;
 //循环记录表中未交易完数据，比较现在价格与$A['sell']，差价大于10%（待定），则卖出
 
 foreach ($icoModel->getIncomplete() as $order) {
 	if (($order['price'] * (1 + 10 / 100)) < $A->sell) {
 		//sell
-		$params = array('api_key' => API_KEY, 'symbol' => 'eth_usd', 'type' => 'sell_market', 'amount' => $order['quanlity']);
+		$params = array('api_key' => API_KEY, 'symbol' => 'eth_cny', 'type' => 'sell_market', 'amount' => $order['quanlity']);
 		$result = $ico -> tradeApi(getParam($params));
 		if ('true' == $result->result) {
-			// $params = array('api_key' => API_KEY, 'symbol' => 'eth_usd', 'order_id' => $result->order_id);
+			// $params = array('api_key' => API_KEY, 'symbol' => 'eth_cny', 'order_id' => $result->order_id);
 			// $return = $ico -> orderInfoApi(getParam($params));
 			$data = array();
 			$data = array('quanlity' => $order['quanlity'], 'price' => $A->sell, 'transac_id' => $order['transac_id']);
@@ -32,11 +31,11 @@ $lastBuy = $icoModel->getLastBuy();
 //比较$A['buy']与B，差价大于10%（待定），则买入
 if (($lastBuy * (1 - 10 / 100)) >= $A->buy){
 	//buy
-	$params = array('api_key' => API_KEY, 'symbol' => 'eth_usd', 'type' => 'buy_market', 'price' => (0.1 * $A->buy));
+	$params = array('api_key' => API_KEY, 'symbol' => 'eth_cny', 'type' => 'buy_market', 'price' => (0.1 * $A->buy));
 	$result = $ico -> tradeApi(getParam($params));
 	if ('true' == $result->result) {
 		$data = array();
-		$data = array('type' => 'eth_usd', 'quanlity' => 0.1, 'price' => $A->buy, 'order_id' => $result->order_id);
+		$data = array('type' => 'eth_cny', 'quanlity' => 0.1, 'price' => $A->buy, 'order_id' => $result->order_id);
 		$icoModel->sell($data);
 	}
 }
