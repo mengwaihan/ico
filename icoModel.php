fa9e01fea5ba228243973842f209ff4db59f070c
zjf580 <zjf@163.com>
2017-09-08 19:52:15 +0800

    test

--- icoModel.php
+++ icoModel.php
@@ -2,9 +2,11 @@
 
 
 class icoModel{
+    protected $ico;
+    protected $db;
     function __construct($authentication) {
-        var_dump($GLOBALS['ico']);
-        var_dump($GLOBALS['db']);
+        $this->ico = $GLOBALS['ico'];
+        $this->db = $GLOBALS['db'];
 
     }
 
@@ -13,7 +15,7 @@ class icoModel{
     	$sql = "SELECT *
     			FROM ico_transac
     			WHERE quanlity > 0";
-    	return $db->getAll($sql);
+    	return $this->db->getAll($sql);
     }
 
 
@@ -24,15 +26,15 @@ class icoModel{
     			WHERE type = 'eth_cny'
                 AND quanlity > 0
     			ORDER BY record_id DESC";
-        $price = $db->getOne($sql);
+        $price = $this->db->getOne($sql);
         if (!$price) {
             $sql = "SELECT price
                 FROM ico_record
                 WHERE type = 'sell_market'
                 ORDER BY record_id DESC";
-            $price = $db->getOne($sql);
+            $price = $this->db->getOne($sql);
         }
-		return $db->getOne($sql);
+		return $this->db->getOne($sql);
     }
 
     //sell
@@ -41,7 +43,7 @@ class icoModel{
     	$sql = "UPDATE ico_transac SET
     			quanlity = :quanlity
     			WHERE transac_id = :transac_id";
-    	$db->execute($sql, array('quanlity' => 0, 'transac_id' => $data['transac_id']));
+    	$this->db->execute($sql, array('quanlity' => 0, 'transac_id' => $data['transac_id']));
     	$data['type'] = 'sell_market';
     	$this->insertRecord($data);
     }
@@ -54,8 +56,8 @@ class icoModel{
     			price = :price,
     			quanlity = :quanlity,
     			order_id = :order_id";
-    	$db->execute($sql, $data);
-    	$data['transac_id'] = $db->lastInsertId();
+    	$this->db->execute($sql, $data);
+    	$data['transac_id'] = $this->db->lastInsertId();
     	unset($data['order_id']);
     	$data['type'] = 'buy_market';
     	$this->insertRecord($data);
@@ -69,13 +71,13 @@ class icoModel{
     			quanlity = :quanlity,
     			price = :price,
     			transac_id = :transac_id";
-    	$db->execute($sql, $data);
+    	$this->db->execute($sql, $data);
     }
 
     public function getOrderInfo ($orderId)
     {
         $params = array('api_key' => API_KEY, 'symbol' => 'eth_cny', 'order_id' => $orderId);
-        $return = $ico -> orderInfoApi($params);
+        $return = $this->ico -> orderInfoApi($params);
 
         return $return->orders[0];
     }
